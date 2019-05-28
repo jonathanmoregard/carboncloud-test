6b3f4c09949c14399d8e111a0bb90dd02148ab35
Jonathan Moregård <jonathan.moregard@solidsystems.se>
2019-05-28 10:17:39 +0200

    Added property test to check functionality, and implemented functionality

--- src/Tree.hs
+++ src/Tree.hs
@@ -1,5 +1,8 @@
 module Tree where
 
+import Data.Char
+import Data.List
+
 data NodeInfo = NodeInfo
   { cost :: Cost
   , nodeInfoName :: NodeName
@@ -24,5 +27,24 @@ data TypeB =
         NodeName
         [TypeB]
 
+getNodeNames :: Tree -> [NodeName]
+getNodeNames (Tree_TypeB b) = getNodeNamesFromB b
+getNodeNames (Tree_TypeA i _ []) = [nodeInfoName i]
+getNodeNames (Tree_TypeA i _ ns) =
+  nodeInfoName i : concat (fmap getNodeNames ns)
+
+getNodeNamesFromB :: TypeB -> [NodeName]
+getNodeNamesFromB (TypeB _ n []) = [n]
+getNodeNamesFromB (TypeB _ n bs) = n : concat (fmap getNodeNamesFromB bs)
+
+-- Returns unique node names that exists in both trees, that doesn't have the string "bepa" in them
+-- Removes duplicates since we consider duplicate names to be instances of the same platonic form
+-- In practice, we would have asked the domain experts what the intended functionality is..
 getCommonNodeNamesExceptBepa :: Tree -> Tree -> [NodeName]
-getCommonNodeNamesExceptBepa _ _ = []
+getCommonNodeNamesExceptBepa t t' =
+  nub $ filter notBepa $ intersect (getNodeNames t) (getNodeNames t')
+
+notBepa :: NodeName -> Bool
+notBepa (NodeName n) = not $ isInfixOf "bepa" $ lower n
+  where
+    lower = map toLower
--- tests/tests.hs
+++ tests/tests.hs
@@ -1,9 +1,8 @@
 module Main where
 
+import Data.List
 import Test.Hspec
-import Test.Hspec.QuickCheck
-import Test.QuickCheck.Arbitrary
-import Test.QuickCheck.Gen
+import Test.QuickCheck
 import Tree
 
 main :: IO ()
@@ -12,41 +11,29 @@ main =
     describe "getCommonNodeNamesExceptBepa does what we want" $ do
       it "returns [] for two Trees with node name 'cepa' but no 'bepa'" $ -- We want this one to fail
        do getCommonNodeNamesExceptBepa cepaButNoBepa cepaButNoBepa `shouldBe` []
+      it "Returns all names found in both trees that doesn't contain 'bepa'" $ do
+        property $ prop_returnsAllNamesFoundInBothTrees
 
 cepaButNoBepa :: Tree
 cepaButNoBepa =
   Tree_TypeA NodeInfo {cost = Cost 0, nodeInfoName = NodeName "cepa"} "" []
 
-genTreeDepth :: Int
-genTreeDepth = 4
+prop_returnsAllNamesFoundInBothTrees :: Property
+prop_returnsAllNamesFoundInBothTrees =
+  forAll
+    genNodeNames
+    (\nns -> do
+       t1 <- genTreeFromNodeNames nns
+       t2 <- genTreeFromNodeNames nns
+       let distincNames = nub nns
+       let combined = getCommonNodeNamesExceptBepa t1 t2
+       return $ combined == distincNames)
 
-genTree :: Gen Tree
-genTree = go genTreeDepth
-  where
-    go 0 = do
-      oneof [genTypeANode [], Tree_TypeB <$> (genTypeBTree 0)]
-    go n = do
-      oneof
-        [ (listOf $ go (n - 1)) >>= genTypeANode
-        , Tree_TypeB <$> (genTypeBTree n)
-        ]
+nodeNamesToGenerate :: Int
+nodeNamesToGenerate = 8
 
-genTypeANode :: [Tree] -> Gen Tree
-genTypeANode cs = do
-  s <- arbitrary
-  c <- arbitrary
-  n <- genNodeName
-  return $ Tree_TypeA NodeInfo {cost = Cost c, nodeInfoName = n} s cs
-
-genTypeBTree :: Int -> Gen TypeB
-genTypeBTree 0 = genTypeB []
-genTypeBTree n = (listOf $ genTypeBTree (n - 1)) >>= genTypeB
-
-genTypeB :: [TypeB] -> Gen TypeB
-genTypeB cs = do
-  c <- arbitrary
-  n <- genNodeName
-  return $ TypeB (Cost c) n cs
+genNodeNames :: Gen [NodeName]
+genNodeNames = vectorOf nodeNamesToGenerate genNodeName
 
 genNodeName :: Gen NodeName
 genNodeName = do
@@ -55,3 +42,32 @@ genNodeName = do
   return $ NodeName [f, s] -- 4 x 4 = 16 permutations
   where
     genChar = elements ['a' .. 'd']
+
+genTreeFromNodeNames :: [NodeName] -> Gen Tree -- wet, should reuse genTreeWith. Hard since typeB can't have typeA children
+genTreeFromNodeNames [] =
+  error "genTreeFromNodeNames: Need at least one element" -- test function, edge case is ok
+genTreeFromNodeNames (n:[]) = do
+  oneof [genTypeANode n [], Tree_TypeB <$> (genTypeBTree [n])]
+genTreeFromNodeNames (n:ns) = do
+  oneof
+    [ (genTypeANode n) =<< (vectorOf 2 $ genTreeFromNodeNames ns)
+    , Tree_TypeB <$> (genTypeBTree $ n : ns)
+    ]
+
+genTypeANode :: NodeName -> [Tree] -> Gen Tree
+genTypeANode n cs = do
+  s <- arbitrary
+  c <- arbitrary
+  return $ Tree_TypeA NodeInfo {cost = Cost c, nodeInfoName = n} s cs
+
+genTypeBTree :: [NodeName] -> Gen TypeB
+genTypeBTree = genTreeWith go
+  where
+    go n cs = do
+      c <- arbitrary
+      return $ TypeB (Cost c) n cs
+
+genTreeWith :: (NodeName -> [a] -> Gen a) -> [NodeName] -> Gen a
+genTreeWith _ [] = error "genTreeWith: Need at least one element" -- test function, edge case is ok
+genTreeWith f (n:[]) = f n []
+genTreeWith f (n:ns) = (f n) =<< (vectorOf 2 $ genTreeWith f ns)
